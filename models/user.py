6ff47af154b35ca88f259f748cc3b9f92d8095e9
Ohazulume C. Fabian <fabianohaz@gmail.com>
2022-09-05 01:56:34 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
-"""
-Module user.py
-Represents a User
-Class User that inherits from BaseModel
-"""
-
+"""User class to inherit from BaseModel"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
     """
-    Public class attributes
-        email: string - empty string
-        password: string - empty string
-        first_name: string - empty string
-        last_name: string - empty string
+    User class to inherit from BaseModel
     """
-
     email = ""
     password = ""
     first_name = ""
